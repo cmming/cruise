1c0d20d38b110f6fbacd2a02a7d28b66c769f88b
chmi <294225707@qq.com>
2019-09-09 10:30:49 +0800

    1.修复语言包

--- src/modules/agent/lang/en.js
+++ src/modules/agent/lang/en.js
@@ -1,15 +1,11 @@
 export default {
     langKey: "agent",
     lang: {
-        header:{
-            'Profile':'详情',
-            'Sign Out':'退出',
-        },
-        menu:{
-            "DASHBOARD":"首页",
-            "AGENT":"AGENT",
-            "MY CRUISE":"MY CRUISE",
-            "HELP":"帮助",
-        }
+        Building: "Building",
+        Idle: "Idle",
+        ALL:"ALL",
+        PHYSICAL:"PHYSICAL",
+        VIRTUAL:"VIRTUAL",
+        Deny:"Deny"
     }
 }
